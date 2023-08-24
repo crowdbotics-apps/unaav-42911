1ecda8a2a78976106d9d26ff214dc6fa4c39b6d9
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-08-24 21:57:51 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "unav-42911",
-  url: "https://unav-42911.botics.co",
-  api: "https://unav-42911.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "unav-42911",
+  "url": "https://unav-42911.botics.co",
+  "api": "https://unav-42911.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
