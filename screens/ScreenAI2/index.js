6a29360a52e5a2183332fdd20cac810e38938e41
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-08-24 22:22:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI2/index.js
@@ -0,0 +1,66 @@
+import { Pressable } from "react-native";
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Animated } from 'react-native';
+
+const SplashScreen = ({
+  navigation
+}) => {
+  const fadeAnim = new Animated.Value(0);
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true
+    }).start(() => navigation.navigate('LoginScreen'));
+  }, [fadeAnim]);
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Animated.Image style={{ ...styles.logo,
+        opacity: fadeAnim
+      }} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Animated.Text style={{ ...styles.text,
+        opacity: fadeAnim
+      }}>
+          Welcome to Our App
+        </Animated.Text>
+      <Pressable><Text style={styles.GeGiLGLX}>{"UNav"}</Text></Pressable></View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 200,
+    height: 200
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  GeGiLGLX: {
+    width: 129,
+    height: 63,
+    lineHeight: 14,
+    fontSize: 24,
+    borderRadius: 0,
+    flexDirection: "row",
+    flex: 1,
+    position: "absolute",
+    left: -42,
+    top: 13
+  }
+});
+export default SplashScreen;
---
+++ screens/ScreenAI3/index.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+
+const OnboardingScreen = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [{
+    image: 'https://tinyurl.com/42evm3m3',
+    text: 'Welcome to our App!'
+  }, {
+    image: 'https://tinyurl.com/42evm3m3',
+    text: 'Enjoy the features we offer.'
+  }, {
+    image: 'https://tinyurl.com/42evm3m3',
+    text: 'Get started now!'
+  }];
+
+  const handleNext = () => {
+    if (currentSlide < slides.length - 1) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  return <SafeAreaView style={styles.container}>
+      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
+        {slides.map((slide, index) => <View key={index} style={styles.slide}>
+            <Image source={{
+          uri: slide.image
+        }} style={styles.image} />
+            <Text style={styles.text}>{slide.text}</Text>
+          </View>)}
+      </ScrollView>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity onPress={handlePrevious} style={styles.button}>
+          <Text style={styles.buttonText}>Previous</Text>
+        </TouchableOpacity>
+        {currentSlide === slides.length - 1 ? <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Get Started</Text>
+          </TouchableOpacity> : <TouchableOpacity onPress={handleNext} style={styles.button}>
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableOpacity>}
+      </View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  slide: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '50%',
+    resizeMode: 'cover'
+  },
+  text: {
+    fontSize: 24,
+    color: '#000',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 5
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18
+  }
+});
+export default OnboardingScreen;
